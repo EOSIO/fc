147ad542793304a6e0b77d393e3cefdb1e364819
Kevin Heifner <heifnerk@objectcomputing.com>
2019-06-27 16:32:48 -0500

    Make log configuration thread safe so that loggers can be updated without restarting application

--- include/fc/log/appender.hpp
+++ include/fc/log/appender.hpp
@@ -36,15 +36,6 @@ namespace fc {
       public:
          typedef std::shared_ptr<appender> ptr;
 
-         template<typename T>
-         static bool register_appender(const fc::string& type) {
-            return register_appender( type, std::make_shared<detail::appender_factory_impl<T>>() );
-         }
-
-         static appender::ptr create( const fc::string& name, const fc::string& type, const variant& args  );
-         static appender::ptr get( const fc::string& name );
-         static bool          register_appender( const fc::string& type, const appender_factory::ptr& f );
-
          virtual void initialize( boost::asio::io_service& io_service ) = 0;
          virtual void log( const log_message& m ) = 0;
    };
