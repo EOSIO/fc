633e258f8a99b9b427e9be09d08ce85a7173fe2c
Matthieu Vachon <matt@dfuse.io>
2020-03-19 11:14:27 -0400

    Improved error handling when facing impossibility to write out the log line

--- src/log/dmlog_appender.cpp
+++ src/log/dmlog_appender.cpp
@@ -16,6 +16,7 @@
 namespace fc {
    class dmlog_appender::impl {
       public:
+         bool is_stopped;
          boost::asio::io_service* io_service;
          boost::mutex log_mutex;
    };
@@ -37,6 +38,12 @@ namespace fc {
 
       string message = format_string( "DMLOG " + m.get_format() + "\n", m.get_data() );
       std::unique_lock<boost::mutex> lock(my->log_mutex);
+      if (my->is_stopped) {
+         // It might happen that `io_server->stop` was called due to printing errors but did not take
+         // effect just yet. So if we are stopped, print a line that we are terminated and return.
+         fprintf(out, "DMLOG FPRINTF_FAILURE_TERMINATED\n");
+         return;
+      }
 
       int retries = 0;
       auto remaining_size = message.size();
@@ -50,8 +57,11 @@ namespace fc {
          fprintf(stderr, "DMLOG FPRINTF_FAILED failed written=%lu remaining=%lu %d %s\n", written, remaining_size, ferror(out), strerror(errno));
 
          if (retries++ > 5) {
-            fprintf(stderr, "DMLOG FPRINTF_FAILED enough is enough\n");
+            fprintf(stderr, "DMLOG FPRINTF_FAILURE_TERMINATED\n");
             my->io_service->stop();
+            my->is_stopped = true;
+
+            return;
          }
 
          message_ptr = &message_ptr[written];
