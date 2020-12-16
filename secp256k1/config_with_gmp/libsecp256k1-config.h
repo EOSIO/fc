251bcf9d390bbeb2864ea92351e806265ac5ad62
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2020-12-16 17:31:09 -0500

    replace old libsecp256k1 fork with upstream

--- secp256k1/config_with_gmp/libsecp256k1-config.h
+++ secp256k1/config_with_gmp/libsecp256k1-config.h
@@ -1,19 +1,15 @@
 #pragma once
 
-//optimizations that any compiler we target have
-#define HAVE_BUILTIN_CLZLL 1
-#define HAVE_BUILTIN_EXPECT 1
-#define HAVE___INT128 1
+#define ENABLE_MODULE_RECOVERY 1
+
+#define ECMULT_GEN_PREC_BITS 4
+#define ECMULT_WINDOW_SIZE 15
 
 //use GMP for bignum
 #define HAVE_LIBGMP 1
 #define USE_NUM_GMP 1
 #define USE_FIELD_INV_NUM 1
 #define USE_SCALAR_INV_NUM 1
-
-//use impls best for 64-bit
-#define USE_FIELD_5X52 1
-#define USE_SCALAR_4X64 1
 
 //enable asm
 #ifdef __x86_64__
