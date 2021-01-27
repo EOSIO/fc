55bc2ad62a3e0eb44dbe3f8464447190b0e1dbba
James Folk <james.folk@block.one>
2021-01-27 16:17:09 -0500

    First attempt at fixing the eof of cfile

--- include/fc/io/cfile.hpp
+++ include/fc/io/cfile.hpp
@@ -24,15 +24,17 @@ class cfile_datastream;
  */
 class cfile {
 public:
-   cfile()
-     : _file(nullptr, &fclose)
+   cfile() :
+   _open(false),
+   _file_path(""),
+   _file(nullptr, &fclose)
    {}
 
-   void set_file_path( fc::path file_path ) {
+   void set_file_path( const fc::path &file_path ) {
       _file_path = std::move( file_path );
    }
 
-   fc::path get_file_path() const {
+   const fc::path &get_file_path() const {
       return _file_path;
    }
 
@@ -47,18 +49,28 @@ public:
    ///         "ab+" - open for binary update - create if does not exist
    ///         "rb+" - open for binary update - file must exist
    void open( const char* mode ) {
+       if(_open)
+           fclose(_file.get());
+
       _file.reset( FC_FOPEN( _file_path.generic_string().c_str(), mode ) );
       if( !_file ) {
          throw std::ios_base::failure( "cfile unable to open: " +  _file_path.generic_string() + " in mode: " + std::string( mode ) );
       }
+
       _open = true;
    }
 
    long tellp() const {
-      return ftell( _file.get() );
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
+       return ftell( _file.get() );
    }
 
    void seek( long loc ) {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
       if( 0 != fseek( _file.get(), loc, SEEK_SET ) ) {
          throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
                                        " unable to SEEK_SET to: " + std::to_string(loc) );
@@ -66,21 +78,40 @@ public:
    }
 
    void seek_end( long loc ) {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
       if( 0 != fseek( _file.get(), loc, SEEK_END ) ) {
          throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
                                        " unable to SEEK_END to: " + std::to_string(loc) );
       }
    }
 
+   void skip( long loc) {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
+       if( 0 != fseek( _file.get(), loc, SEEK_CUR ) ) {
+           throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
+                                          " unable to SEEK_CUR to: " + std::to_string(loc) );
+       }
+   }
+
    void read( char* d, size_t n ) {
-      size_t result = fread( d, 1, n, _file.get() );
-      if( result != n ) {
-         throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
-                                       " unable to read " + std::to_string( n ) + " bytes; only read " + std::to_string( result ) );
-      }
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
+       size_t result = fread( d, 1, n, _file.get() );
+        if( result != n ) {
+            throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
+            " unable to read " + std::to_string( n ) + " bytes; only read " + std::to_string( result ) );
+        }
    }
 
    void write( const char* d, size_t n ) {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
       size_t result = fwrite( d, 1, n, _file.get() );
       if( result != n ) {
          throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
@@ -89,6 +120,9 @@ public:
    }
 
    void flush() {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
       if( 0 != fflush( _file.get() ) ) {
          int ec = ferror( _file.get() );
          throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
@@ -97,6 +131,9 @@ public:
    }
 
    void sync() {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
       const int fd = fileno(_file.get() );
       if( -1 == fd ) {
          throw std::ios_base::failure( "cfile: " + _file_path.generic_string() +
@@ -109,9 +146,35 @@ public:
       }
    }
 
+   int getc() {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
+       int ret = fgetc(_file.get());
+
+       if (ret == EOF) {
+           throw std::ios_base::failure( "cfile: " + _file_path.generic_string() + " unable to read 1 byte");
+       }
+       return ret;
+    }
+
    void close() {
-      _file.reset();
-      _open = false;
+       if(_open) {
+           _file.reset();
+           _open = false;
+       }
+   }
+
+   bool eof() const  {
+       if(!_open)
+           throw std::ios_base::failure("cfile is not open");
+
+       if(feof(_file.get()))return true;
+
+       int c = fgetc(_file.get());
+       if(EOF == c)return true;
+
+       return (EOF == ungetc(c, _file.get()));
    }
 
    cfile_datastream create_datastream();
--- test/io/test_cfile.cpp
+++ test/io/test_cfile.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE io
 #include <boost/test/included/unit_test.hpp>
+#include <boost/filesystem/fstream.hpp>
 
 #include <fc/io/cfile.hpp>
 
@@ -56,6 +57,46 @@ BOOST_AUTO_TEST_SUITE(cfile_test_suite)
       t.close();
       fc::remove_all( t.get_file_path() );
       BOOST_CHECK( !fc::exists( tempdir.path() / "test") );
+   }
+
+   BOOST_AUTO_TEST_CASE(test_eof)
+   {
+    fc::temp_file tmpfile;
+    {
+        boost::filesystem::ofstream ofs(tmpfile.path());
+        ofs << "the quick brown fox";
+    }
+
+    cfile t;
+    t.set_file_path(tmpfile.path());
+    BOOST_CHECK_NO_THROW(t.open("r"));
+    BOOST_CHECK(t.is_open());
+    BOOST_CHECK_NO_THROW(!t.eof());
+    BOOST_CHECK_NO_THROW(t.getc() == 't');
+    BOOST_CHECK_NO_THROW(t.skip(10));
+    BOOST_CHECK_NO_THROW(!t.eof());
+    BOOST_CHECK_NO_THROW(t.getc() == 'r');
+    BOOST_CHECK_NO_THROW(t.skip(7));
+    BOOST_CHECK_NO_THROW(t.eof());
+    BOOST_CHECK_THROW(t.getc(), std::ios_base::failure);
+
+    BOOST_CHECK_NO_THROW(t.open("r"));
+    BOOST_CHECK_NO_THROW(!t.eof());
+    BOOST_CHECK_NO_THROW(t.skip(30));
+    BOOST_CHECK_NO_THROW(t.eof());
+
+    BOOST_CHECK_NO_THROW(t.open("r"));
+    BOOST_CHECK_NO_THROW(!t.eof());
+    BOOST_CHECK_THROW(t.skip(-2), std::ios_base::failure);
+
+    BOOST_CHECK_NO_THROW(t.open("r"));
+    BOOST_CHECK_NO_THROW(!t.eof());
+    BOOST_CHECK_NO_THROW(t.skip(4));
+    BOOST_CHECK_NO_THROW(!t.eof());
+    BOOST_CHECK_NO_THROW(t.getc() == 'q');
+    BOOST_CHECK_NO_THROW(t.skip(19));
+    BOOST_CHECK_NO_THROW(t.eof());
+    BOOST_CHECK_THROW(t.seek(25), std::ios_base::failure);
    }
 
 BOOST_AUTO_TEST_SUITE_END()
